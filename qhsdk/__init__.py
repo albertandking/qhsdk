abe78a3d0212e9e24cf5fca35cd0893df7385443
jindaxiang <jindaxiang@163.com>
2019-11-10 18:12:31 +0800

    Initial commit

---
+++ qhsdk/__init__.py
@@ -0,0 +1,17 @@
+"""qhsdk 是基于 Python 的为奇货可查客户服务的专用API接口"""
+
+"""
+
+"""
+
+__version__ = '0.0.1'
+__author__ = 'qhkch'
+
+
+"""
+大宗商品展期收益率数据
+"""
+from qhsdk.futures.roll_yield import (get_roll_yield_bar,
+                                      get_roll_yield)
+
+
